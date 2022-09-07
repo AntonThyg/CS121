9f229a12bce9386512b13e777f5d75392eee978d
AntonThyg <89147426+AntonThyg@users.noreply.github.com>
2022-09-07 14:03:07 -0400

    Completed activity 4

--- src/Chapter_1_to_2/Activity_4/PlanetsSwitch.java
+++ src/Chapter_1_to_2/Activity_4/PlanetsSwitch.java
@@ -1,3 +1,12 @@
+//**********************************************************************************************************************
+//                              Activity 4
+//                              Name: Antone Thygerson
+//                              Date: 9/07/2022
+//**********************************************************************************************************************
+//  User enters the name of a planet. Program prints the planet's mass
+//
+//
+//**********************************************************************************************************************
 package Chapter_1_to_2.Activity_4;
 
 import java.util.Scanner;
@@ -7,7 +16,27 @@ public class PlanetsSwitch {
         Scanner ask = new Scanner(System.in);
         System.out.println("Planet: ");
         String planet = ask.next();
-
-
+        String mass = "";
+        switch (planet){
+            case "mercury":
+                mass = "3.285e23";
+            case "venus":
+                mass = "4.867e24";
+            case "earth":
+                mass = "5.972e24";
+            case "mars":
+                mass = "6.39e23";
+            case "jupiter":
+                mass = "1.898e27";
+            case "saturn":
+                mass = "5.683e26";
+            case "neptune":
+                mass = "1.024e26";
+            case "uranus":
+                mass = "8,681e25";
+            case "pluto":
+                mass = "1.309e22";
+        }
+        System.out.printf("The mass of %s is %s",planet,mass);
     }
 }
